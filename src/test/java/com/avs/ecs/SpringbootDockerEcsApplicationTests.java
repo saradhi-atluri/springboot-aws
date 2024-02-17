875a57752d09c13efef8a6e20d0fcdd8928cf6af
Vijaya Saradhi Atluri <saradhi@Vijayas-MacBook-Pro.local>
2024-02-17 12:50:53 -0500

    Initial commit

---
+++ src/test/java/com/avs/ecs/SpringbootDockerEcsApplicationTests.java
@@ -0,0 +1,13 @@
+package com.avs.ecs;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class SpringbootDockerEcsApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
